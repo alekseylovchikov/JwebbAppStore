3bb08dfab844e9aa37e47cdd9170e2d9b6ec1fca
Aleksey Lovchikov <lovchikov.a3@wb.ru>
2024-09-14 21:11:34 +0300

    Initial commit

--- JwebbAppStore/ContentView.swift
+++ JwebbAppStore/ContentView.swift
@@ -9,16 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    HStack(spacing: 16) {
+                        RoundedRectangle(cornerRadius: 16).frame(width: 80, height: 80)
+                        
+                        VStack(alignment: .leading) {
+                            Text("Youtube: Watch, Listen, blah, blah, blah")
+                                .lineLimit(1).font(.system(size: 20))
+                            Text("Photo & Video").foregroundStyle(Color(.gray))
+                            Text("STARS 32.0M")
+                        }
+                        
+                        Image(systemName: "icloud.and.arrow.down")
+                    }
+                    HStack {
+                        RoundedRectangle(cornerRadius: 12).frame(height: 200)
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle("Search")
+            .searchable(text: .constant("Enter search term"))
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
 }
