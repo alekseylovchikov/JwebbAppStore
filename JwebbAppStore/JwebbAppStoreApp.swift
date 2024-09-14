afb3aa3a78c34a8ec47c81692aa0775d567377c7
Aleksey Lovchikov <lovchikov.a3@wb.ru>
2024-09-14 20:39:34 +0300

    Initial Commit

---
+++ JwebbAppStore/JwebbAppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  JwebbAppStoreApp.swift
+//  JwebbAppStore
+//
+//  Created by Aleksey Lovchikov on 14/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct JwebbAppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
